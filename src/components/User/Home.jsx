b1a2290ab3b5f5097cb22270575a69d258c64719
Agathius <p.akos1148@gmail.com>
2023-06-09 23:56:51 +0200

    fixed wonky layout, simplified .css, prettifieed component

--- src/components/User/Home.jsx
+++ src/components/User/Home.jsx
@@ -1,5 +1,6 @@
-// Landing page 
-import React, {useState, useEffect} from 'react'
+// Landing page with photo carousel display 
+import React, { useState, useEffect } from 'react'
+import ReactDOMServer from 'react-dom/server';
 import './Home.css'
 import '../Shared.css'
 import {useFormContext } from '../contexts/FormContext'
@@ -13,15 +14,24 @@ import HomePhoto from './HomePhoto'
 import Button from '../UI/Button'
 import { useNavigate } from 'react-router'
 import { useAuthContext } from '../contexts/AuthenticationContext'
+import { useMediaQuery } from 'react-responsive';
+import { BlobLandscapeBackground, BlobPortraitBackground } from '../SVG/Backgrounds';
+import { useThemeContext } from '../contexts/ThemeContext';
 
 export default function Home() {
+  // CONSTANTS
+  // get css colors vars to pass to svg component as prop
+  const colorMain = getComputedStyle(root).getPropertyValue('--bg-color--main');
+  const colorTernary = getComputedStyle(root).getPropertyValue('--bg-color--ternary-transparent-high');
   // ROUTE
   const navigate =  useNavigate(); 
   // CONTEXT
-  const {homePhotos, setHomePhotos} = useFormContext();
-  const {auth} = useAuthContext();
+  const { homePhotos, setHomePhotos } = useFormContext();
+  const { auth } = useAuthContext();
+  const { theme } = useThemeContext();
   // HOOK
-  const {allImagesLoaded, hideImageStyle, setCurrentlyLoadingImages, getImageFile, onLoadHandler} = useHideImagesWhileLoading();
+  const { allImagesLoaded, hideImageStyle, setCurrentlyLoadingImages, getImageFile, onLoadHandler} = useHideImagesWhileLoading();
+  const isLargeScreen = useMediaQuery({ query: '(min-width: 768px)' });
   // STATE
   const [isLoading, setIsLoading] = useState(true);
   // EFFECT
@@ -33,14 +43,42 @@ export default function Home() {
         setIsLoading(false); 
       })() 
   }, []) 
+
+  useEffect(() => {console.log(colorMain, colorTernary)}, [theme])
+
   // RENDERED ELEMENTS
+  // BACKGROUND
+  // Set up responsive background
+  const backgroundComponents = (
+    <>
+      {isLargeScreen ? (
+        // Landscape for tablet/pc view
+        <BlobLandscapeBackground color1={colorMain} color2={colorTernary} />
+        ) : (
+        // Portrait for mobile view
+        <BlobPortraitBackground  color1={colorMain} color2={colorTernary} />
+      )}
+    </>
+  )
+  // Convert svg component to string 
+  const renderedBackground = encodeURIComponent(ReactDOMServer.renderToString(backgroundComponents));
+  // Define background as inline style
+  const modalBackground = {
+    backgroundPosition: 'center', 
+    backgroundSize: 'cover', 
+    backgroundRepeat: 'no-repeat',
+    backgroundImage: `url("data:image/svg+xml, ${renderedBackground}")`,
+    backgroundColor: 'var(--bg-color--main)'
+  }
+
   // loader
   const loader = (
     <div className='shared-page-container shared-page-container--centered shared-page-container--with-padding'>   
     <LoaderIcon height='100px' width='100px' stroke='var(--text-color--high-emphasis)'/>
     </div>
   )
-  // photo placeholder if fetch was unsuccessfull && no data
+
+  // photo placeholder failed fetch || no data
   const photoPlaceholder = (
     <div className='home-photo'>
       <span> error displaying images </span>
@@ -71,28 +109,33 @@ export default function Home() {
   )
   // home page content
   const home = (
-    <div className='shared-page-container shared-page-container--centered shared-page-container--with-padding'>
-      <div className='home-header'>
-        {/* Header title */}
-        <div className='home-title'>
-          <h1> Photo Gallery </h1>
+    <div style={modalBackground} className='shared-page-container shared-page-container--centered shared-page-container--with-padding'>
+      <div className='home-container'>
+        {/* Header */}
+        <div className='home-header'>
+          {/* title */}
+          <div className='home-title'>
+            <h1> Photo Gallery </h1>
+          </div>
+          {/* subtitle */}
+          <div className='home-subtitle'>
+            <span> footages for you </span>
+          </div>
         </div>
-        {/* Subtitle */}
-        <div className='home-subtitle'>
-          <h2> footages for you </h2>
+        {/* Photo carousel / placeholder */}
+        <div className={`home-photo ${homePhotos && homePhotos.length > 0 ? '' : 'photo-placeholder'}`}>
+          { homePhotos && homePhotos.length > 0 ? photos : photoPlaceholder }
         </div>
-      </div>
-      <div className={`home-photo ${homePhotos && homePhotos.length > 0 ? '' : 'photo-placeholder'}`}>
-        { homePhotos && homePhotos.length > 0 ? photos : photoPlaceholder }
-      </div>
-      {/* Button container */}
-      <div className='home-button'>
-        <Button clicked={() => navigate(auth.userID ? `/${auth.username}/gallery` : '/login')} buttonStyle='button-home-call-to-action'>
-          <span> Visit Gallery </span>
-        </Button>
+        {/* Button container */}
+        <div className='home-button'>
+          <Button clicked={() => navigate(auth.userID ? `/${auth.username}/gallery` : '/login')} buttonStyle='button-home-cta'>
+            <span> Visit Gallery </span>
+          </Button>
+        </div>
       </div>
     </div>
   ) 
+
   return (
     <>
       {isLoading ? loader : home}
