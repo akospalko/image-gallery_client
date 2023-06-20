3d6eb9217c712edd8d29db3c3a760c2981fae9d2
Agathius <p.akos1148@gmail.com>
2023-06-20 09:37:19 +0200

    simplified component, outsourced modals

--- src/components/User/Gallery.jsx
+++ src/components/User/Gallery.jsx
@@ -1,50 +1,30 @@
-// User view photo gallery and user collection logic
-import React from 'react'
-import '../Shared.css'
-import PhotoEntries from './PhotoEntries'
-import UserCollectionPhotoEntries from './UserCollectionPhotoEntries'
-import {OPERATIONS, MODAL_TITLES} from '../../helper/dataStorage'
-import PhotoEntryModal from '../Modals/PhotoEntryModal'
-import { useModalContext } from '../contexts/ToggleModalContext'
-import MapModal from '../Modals/MapModal'
-import FullScreenView from '../Modals/FullScreenView'
-import PhotoInfo from '../Modals/PhotoInfo'
+// User view photo gallery page
+import React from 'react';
+import '../Shared.css';
+import PhotoEntriesGallery from './PhotoEntriesGallery';
+import { useModalContext } from '../contexts/ToggleModalContext';
+import useSetUpModal from '../hooks/useSetUpModal';
+import { CONSTANT_VALUES } from '../../helper/constantValues';
 
-export default function Gallery({isUserCollection}) {
-  // PROPS: isUserCollection - display user collection || gallery
+export default function Gallery() {
   // CONTEXT
-  const {toggleModal} = useModalContext();
+  const { toggleModal } = useModalContext();
+  
+  // HOOK
+  const { mapViewModal, photoViewModal, infoViewModal } = useSetUpModal(toggleModal);
+ 
   return (
     <div className='shared-page-container'>
-      {/* Title */}
-      <h1> {isUserCollection ? 'My Collection' : 'Gallery' } </h1>  
-      {/* Photo entries */}
-      {isUserCollection ? <UserCollectionPhotoEntries/> : <PhotoEntries/>}
-      {/* Modals */}
-      {/* map view */}
-      { toggleModal[OPERATIONS.MAP_VIEW] && 
-        <PhotoEntryModal 
-          modalTitle={ MODAL_TITLES[OPERATIONS.MAP_VIEW] }
-          modalContent={ <MapModal/> } 
-          contentStyle='shared-page-container--with-padding' 
-          closeModal={ OPERATIONS.MAP_VIEW } 
-        /> }
-      {/* Photo view */}
-      { toggleModal[OPERATIONS.FULLSCREEN_VIEW] && 
-        <PhotoEntryModal 
-          modalTitle={ MODAL_TITLES[OPERATIONS.FULLSCREEN_VIEW] }
-          modalContent={ <FullScreenView/> } 
-          contentStyle='shared-modal-content--centered' 
-          closeModal={ OPERATIONS.FULLSCREEN_VIEW } 
-        /> }
-      {/* Photo entry info */}
-      { toggleModal[OPERATIONS.PHOTO_INFO_VIEW] && 
-        <PhotoEntryModal 
-          modalTitle={ MODAL_TITLES[OPERATIONS.PHOTO_INFO_VIEW] }
-          modalContent={ <PhotoInfo displayTimestamp/> } 
-          contentStyle='shared-page-container--with-padding' 
-          closeModal={ OPERATIONS.PHOTO_INFO_VIEW } 
-        /> }
+      { /* Header title */ }
+      <div className='shared-header-wrapper'> 
+        <h1> { CONSTANT_VALUES.TITLE_GALLERY_USER } </h1>  
+      </div> 
+      { /* Photo entries */ }
+      <PhotoEntriesGallery /> 
+      { /* Modals */ }
+      { mapViewModal }
+      { photoViewModal }
+      { infoViewModal } 
     </div>
   )
 }
