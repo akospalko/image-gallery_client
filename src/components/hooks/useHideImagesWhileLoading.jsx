aecfb3e982455a764eb4ffae20311ecd2902f9f2
Agathius <p.akos1148@gmail.com>
2023-07-04 19:21:37 +0200

    fixed hide immgs while loading

--- src/components/hooks/useHideImagesWhileLoading.jsx
+++ src/components/hooks/useHideImagesWhileLoading.jsx
@@ -1,22 +1,20 @@
 // Logic to hide images while they are being loaded (using css)
 // display them when load is done (photos are loaded by the browser) 
 // it uses <img/> tag and onLoad tag attribute to solve the problem. 
-
 import { useEffect, useState } from 'react';
 
 export default function useHideImagesWhileLoading() {
   // STATE
-  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
-  const [currentlyLoadingImages, setCurrentlyLoadingImages] = useState({}); 
+  const [ allImagesLoaded, setAllImagesLoaded ] = useState(false);
+  const [ currentlyLoadingImages, setCurrentlyLoadingImages ] = useState({}); 
 
   // EFFECT
   useEffect(() => {
-    const loadingImagesArr = Object.values(currentlyLoadingImages || {});
-    const isAllImageLoaded = loadingImagesArr.every(img => img === true);
-    isAllImageLoaded === true ? setAllImagesLoaded(isAllImageLoaded) : null;
-    return () => { setAllImagesLoaded(false) };
-  }, [allImagesLoaded, currentlyLoadingImages, setAllImagesLoaded])
-  
+    const loadingImagesArr = Object.values(currentlyLoadingImages || {}); // 
+    const areAllImagesLoaded = loadingImagesArr.length > 0 && loadingImagesArr.every(img => img === true);
+    setAllImagesLoaded(areAllImagesLoaded);
+  }, [ currentlyLoadingImages, setAllImagesLoaded ]);
+
   // HANDLER 
   // update load state to true when image is finished loading 
   const onLoadHandler = (photoID) => {
