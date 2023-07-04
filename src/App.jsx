494d7dbccf926435b147f793c192e20d1a4f0af9
Agathius <p.akos1148@gmail.com>
2023-07-04 22:15:24 +0200

    added data context, new css variables, map fullscreen svg

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ToggleModalContext from './components/contexts/ToggleModalContext'
 import FormContext from './components/contexts/FormContext'
 import AuthenticationContext from './components/contexts/AuthenticationContext'
 import LoaderContext from './components/contexts/LoaderContext'
+import DataContext from './components/contexts/DataContext'
 import ThemeContext from './components/contexts/ThemeContext'
 import { ToastContainer } from 'react-toastify';
 
@@ -15,6 +16,7 @@ function App() {
       <LoaderContext>
       <ToggleModalContext>
       <AuthenticationContext>
+      <DataContext>
       <FormContext>
         {/* Header */}
         <Header/>
@@ -24,16 +26,17 @@ function App() {
         {/* toast like status messages */}
         <ToastContainer
           position="bottom-center"
-          autoClose={4000}
+          autoClose={ 4000 } 
           hideProgressBar
           newestOnTop
           closeOnClick
-          rtl={false}
+          rtl={ false }
           pauseOnFocusLoss
           draggable
           pauseOnHover
         />
       </FormContext>
+      </DataContext>
       </AuthenticationContext>
       </ToggleModalContext>
       </LoaderContext>
