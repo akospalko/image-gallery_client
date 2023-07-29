09a252b7b0fb4a244cf6769b4e5d4dd2a4391427
Agathius <p.akos1148@gmail.com>
2023-07-29 09:55:51 +0200

    restructured home page layout, adjusted fonts, buttons, photo container sizes and related skeleton loaders. Moved http request to context where home photos are stored. Added http request loader, fixed 'couldn't display imgs' message which was showing even before fetch would have ended

--- src/components/User/Home.jsx
+++ src/components/User/Home.jsx
@@ -1,9 +1,7 @@
-// Landing page with photo carousel display 
-import React, { useEffect } from 'react'
+// Landing/home page with photo carousel display 
+import React from 'react'
 import './Home.css'
 import '../Shared.css'
-import { getAllHomePhotos } from '../../helper/axiosRequests'
-import axios from '../../helper/axiosInstances'
 import useHideImagesWhileLoading from '../hooks/useHideImagesWhileLoading'
 import SkeletonHome from '../../skeletons/SkeletonHome'
 import Carousel from './Carousel'
@@ -16,44 +14,37 @@ import { useDataContext } from '../contexts/DataContext';
 import { CONSTANT_VALUES } from '../../helper/constantValues'
 
 export default function Home() {
-  // CONSTANTS
   // ROUTE
-  const navigate =  useNavigate(); 
+  const navigate = useNavigate(); 
+  
   // CONTEXT
-  const { homePhotos, setHomePhotos } = useDataContext();
+  const { homePhotos, isLoadingData } = useDataContext();
   const { auth } = useAuthContext();
+  
   // HOOK
   const { allImagesLoaded, hideImageStyle, setCurrentlyLoadingImages, onLoadHandler} = useHideImagesWhileLoading();
   const { pageBackground } = useResponsiveBackground();
 
-  // EFFECT
-  useEffect(() => {  
-  if(homePhotos?.length) return; // fetch photos if photo storage is empty  
-    (async () => {
-      const response = await getAllHomePhotos(axios); // fetch entries, update state
-      response.success ? setHomePhotos(response?.photoEntries) : setHomePhotos(null) // set state with fetched photo-s || []  
-    })() 
-}, []) 
-  
   // photo placeholder failed fetch || no data
   const photoPlaceholder = (
     <div className='home-photo'>
-      <span> { CONSTANT_VALUES.INFO_FAIL_TO_DISPLAY_IMGS } </span>
+      <h3> { CONSTANT_VALUES.INFO_FAIL_TO_DISPLAY_IMGS } </h3>
     </div>
   )
   // photo carousel with rendered home photos
   const carousel = (
-    <Carousel hideImageStyle={hideImageStyle} >
+    <Carousel hideImageStyle={ hideImageStyle } >
       { homePhotos && homePhotos.map(photo => (
         <HomePhoto 
-          key={photo._id} 
-          photo={photo} 
-          onLoadHandler={onLoadHandler}
-          setCurrentlyLoadingImages={setCurrentlyLoadingImages}
+          key={ photo._id } 
+          photo={ photo } 
+          onLoadHandler={ onLoadHandler }
+          setCurrentlyLoadingImages={ setCurrentlyLoadingImages}
         /> 
       )) }
     </Carousel>
   )
+
   // display photo || skeleton loader
   const photos =  ( 
     <>
@@ -63,33 +54,48 @@ export default function Home() {
       { homePhotos && homePhotos.length && carousel }
     </>
   )
- 
+  
+  // ELEMENTS
+  // Header
+  const header = (
+    <div className='home-header'>
+      {/* title */}
+      <div className='home-title'>
+        <h1> { CONSTANT_VALUES.TITLE_HOME_USER } </h1>
+      </div>
+      {/* subtitle */}
+      <div className='home-subtitle'>
+        <h3> { CONSTANT_VALUES.SUBTITLE_HOME_USER } </h3>
+      </div>
+    </div>
+  )
+  // Carousel photos || placeholder
+  const displayedPhotos = (
+    <div className={ `home-photo ${ isLoadingData && homePhotos && homePhotos?.length > 0 ? '' : 'photo-placeholder' }` }>
+      { isLoadingData ? <SkeletonHome/> : homePhotos && homePhotos?.length > 0 ? photos : photoPlaceholder }
+    </div>
+  )
+
+  // CTA button
+  const ctaButton = (
+    <div className='home-button'>
+      <Button clicked={ () => navigate(auth.userID ? `/${ auth.username }/gallery` : '/login') } buttonStyle='button-home-cta'>
+        <span> { CONSTANT_VALUES.BUTTON_VISIT_GALLERY } </span>
+      </Button>
+    </div>
+  )
+
   return (
     // home page content
-    <div style={ pageBackground } className='shared-page-container shared-page-container--centered shared-page-container--with-padding'>
+    <article style={ pageBackground } className='shared-page-container shared-page-container--with-padding'>
       <div className='home-container'>
         {/* Header */}
-        <div className='home-header'>
-          {/* title */}
-          <div className='home-title'>
-            <h1> { CONSTANT_VALUES.TITLE_HOME_USER } </h1>
-          </div>
-          {/* subtitle */}
-          <div className='home-subtitle'>
-            <span> { CONSTANT_VALUES.SUBTITLE_HOME_USER } </span>
-          </div>
-        </div>
+        { header }
         {/* Photo carousel / placeholder */}
-        <div className={ `home-photo ${ homePhotos && homePhotos.length > 0 ? '' : 'photo-placeholder' }` }>
-          { homePhotos && homePhotos.length > 0 ? photos : photoPlaceholder }
-        </div>
+        { displayedPhotos }
         { /* Button container */ }
-        <div className='home-button'>
-          <Button clicked={ () => navigate(auth.userID ? `/${ auth.username }/gallery` : '/login') } buttonStyle='button-home-cta'>
-            <span> { CONSTANT_VALUES.BUTTON_VISIT_GALLERY } </span>
-          </Button>
-        </div>
+        { ctaButton }
       </div>
-    </div> 
+    </article> 
   )
 }
